5d944d487b14f3c858e3d7c8972645943dc27410
becca-mcd-95 <becca.mcdonagh@hotmail.co.uk>
2026-02-25 19:43:04 +0000

    Becca committing updated set ups

--- cooked_project/urls.py
+++ cooked_project/urls.py
@@ -1,4 +1,4 @@
-"""cooked URL Configuration
+"""cooked_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cooked import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
