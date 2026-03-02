84d037afd284a1aa622eb4a8c162c09512755947
becca-mcd-95 <becca.mcdonagh@hotmail.co.uk>
2026-03-02 20:01:39 +0000

    Created base user profile model (will need edited) Created database and superuser Added bootstrap to all template pages

--- cooked/models.py
+++ cooked/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# this class was written with help as a placeholder - come back to understand and edit it 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    # do we need username and name field? does that pull from database? 
+    bio = models.TextField(max_length=400, blank=True)
+    city = models.TextField(max_length=40, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_photos', blank=True, null=True)
+    header_photo = models.ImageField(upload_to='header_photos', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
