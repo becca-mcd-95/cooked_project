aa7ca538ae41baca21da2da44ecb5258e078d023
becca-mcd-95 <becca.mcdonagh@hotmail.co.uk>
2026-03-03 17:49:13 +0000

    added CSS successfully this time, added red bg for testing

--- cooked_project/settings.py
+++ cooked_project/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_DIR = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/cooked/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
