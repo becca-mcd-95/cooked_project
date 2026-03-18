9f9cd2e7aa7fcb3fa5c225f217c1cdebf5207324
becca-mcd-95 <becca.mcdonagh@hotmail.co.uk>
2026-03-18 19:24:08 +0000

    more app.py editing...

--- cooked/apps.py
+++ cooked/apps.py
@@ -4,24 +4,4 @@ class CookedConfig(AppConfig):
     name = 'cooked'
 
     def ready(self):
-        from cooked.models import create_user_profile
-        create_user_profile()
-
-
-"""
-Commenting out the below because I'm not sure what it influences 
-
-"""
-
-# # Yanyan
-
-# class RecipesConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "recipes"
-
-# # Tuoyu
-
-# class SocialConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "social"
-
+        import cooked.models
